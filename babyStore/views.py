c8aaa6ab8b6d8cc9def3a49d9e0363cb63b64a9a
Pinky <janeletageng28@gmail.com>
2021-02-08 00:48:07 +0200

    I am finished

--- babyStore/views.py
+++ babyStore/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
 import json
 import datetime
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 from django.views.static import serve
 from django.conf.urls import url
@@ -11,6 +12,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('babyStore.urls')),
+
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 
     # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
